354d9ae51445c99e2b11048223c64aea624ef891
Graphic <graphic@SJH-607-04mac.local>
2015-01-16 15:58:14 -0500

    Add initial idead for detection

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	public float speed = 1f;
+	enum Directions {NW, NE, SW, SE};
+	private Directions dir;
 
 	private void Update () {
 
@@ -13,20 +15,30 @@ public class PlayerControl : MonoBehaviour {
 		float x = speed * Time.deltaTime;
 		float y = speed/2 * Time.deltaTime;
 
+		Directions old = dir;
+
 		if (hMove != 0) {
 			if (hMove > 0) {
 				transform.Translate(x,-y,0); // bottom right
+				dir = Directions.SE;
 			} else {
 				transform.Translate(-x,y,0); // top left
+				dir = Directions.NW;
 			}
 		} 
 
 		if (vMove != 0) {
 			if (vMove > 0) {
 				transform.Translate(x,y,0);  // top right
+				dir = Directions.NE;
 			} else {
 				transform.Translate(-x,-y,0); // bottom left
+				dir = Directions.SW;
 			}
+		}
+
+		if (dir != old) {
+			Debug.Log (dir);
 		}
 
 	}
