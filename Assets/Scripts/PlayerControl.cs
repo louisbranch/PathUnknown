4c7c6eb26dd156cc980f712eeb6a1d11e6522737
Graphic <graphic@SJH-607-04mac.local>
2015-01-16 15:33:01 -0500

    Add time based movement, with the right angle

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,38 +3,30 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
-	private float speed = 0.1f;
-	private float move = 0.5f;
-	private float half = 0.25f;
-	private float counter;
-
-	private void Start () {
-		counter = Time.time;
-	}
+	public float speed = 1f;
 
 	private void Update () {
-
-		if ((Time.time - counter) < speed) return;
 
 		float hMove = Input.GetAxis("Horizontal");
 		float vMove = Input.GetAxis("Vertical");
 
+		float x = speed * Time.deltaTime;
+		float y = speed/2 * Time.deltaTime;
+
 		if (hMove != 0) {
 			if (hMove > 0) {
-				transform.Translate(move,-half,0); // bottom right
+				transform.Translate(x,-y,0); // bottom right
 			} else {
-				transform.Translate(-move,half,0); // top left
+				transform.Translate(-x,y,0); // top left
 			}
+		} 
 
-			counter = Time.time;
-		} else if (vMove != 0) {
+		if (vMove != 0) {
 			if (vMove > 0) {
-				transform.Translate(move,half,0);  // top right
+				transform.Translate(x,y,0);  // top right
 			} else {
-				transform.Translate(-move,-half,0); // bottom left
+				transform.Translate(-x,-y,0); // bottom left
 			}
-
-			counter = Time.time;
 		}
 
 	}
