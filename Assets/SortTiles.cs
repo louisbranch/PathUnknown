111aeea08dc76625be2e2b498416a341680e0b94
Graphic <graphic@SJH-607-04mac.local>
2015-01-18 12:35:09 -0500

    Create script to sort the tiles

---
+++ Assets/SortTiles.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortTiles : MonoBehaviour {
+
+	private void Awake(){
+		int order = 0;
+		Transform[] allChildren = GetComponentsInChildren<Transform>();
+		foreach (Transform child in allChildren) {
+			if (child.renderer) child.renderer.sortingOrder = order++;
+		}
+	}
+}
